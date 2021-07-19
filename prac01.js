f5fcd1de4e1c2c13ba10b2e003804f50077f68f7
seungjuoh <sjoh0704@daum.net>
2021-07-19 18:57:14 +0900

    add prac02

---
+++ prac01.js
@@ -0,0 +1,33 @@
+// 노드는 자바스크립트 엔진으로 빌드된 자바스크립트 런타임 
+// v8엔진은 브라우저가 없이도 자바스크립트가 동작할 수 있도록 만든것
+// 즉 v8엔진은 자바스크립트를 실행할 수 잇는 환경 
+// 노드 = 자바스크립트를 브라우저가 아닌 컴퓨터에서 브라우저 없이 실행하게 도와주는 환경 
+// ES6는 자바스크립트에 대한 버전 (많은 곳에서 표준으로 사용)
+// npm = node package manager 파이썬의 Pip와 동일 
+
+// 자바 스크립트 실행 방법
+// node 설치 후, 
+// 터미널에서 node 파일명 
+
+// 변수 선언 
+var number = 10
+console.log(number)
+
+// 리스트 선언 
+var numArray = [10, 20, 30]
+console.log(numArray[0])
+console.log(numArray[1])
+
+//Dictionary
+var person = {'name':'ppanda', 'age': 21}
+console.log(person)
+console.log(person['name'])
+
+// 조건문
+
+if(person['age'] > 19){
+    console.log('술 먹자')
+}
+else{
+    console.log('못 먹어~')
+}
---
+++ prac02.js
@@ -0,0 +1,39 @@
+//함수 사용하기 
+function isValidAge(person){
+    if(person['age'] > 19)
+        return true;
+    else
+        return false;
+}
+
+var personArray = [
+    {'name':'sseung', 'age':25},
+    {'name':'ppanda', 'age':30},
+    {'name':'john', 'age':17},
+    {'name':'cindy', 'age':13}
+]
+
+//for문 사용하기
+for(var i = 0; i < personArray.length; i++){
+    console.log(personArray[i]['name'] + '씨 당신은 ')
+    if(isValidAge(personArray[i])){
+
+        console.log('성인입니다!')
+    }
+    else{
+        console.log('미성년자입니다!')
+    }
+}
+   
+
+// 나이 평균 구하는 함수 만들기
+
+function getAverageAge(personArray){
+    sum = 0
+    for(var i=0; i<personArray.length; i++){
+        sum += personArray[i]['age']
+    }
+    return sum / personArray.length;
+}
+
+console.log(getAverageAge(personArray))
