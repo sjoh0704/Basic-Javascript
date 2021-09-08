c1d1bff91e44d27a75089f59d50fcbafd40894fe
seungjuoh <sjoh0704@daum.net>
2021-09-08 13:40:29 +0900

    add jest

---
+++ validation.js
@@ -0,0 +1,15 @@
+module.exports = {
+    isEmail: (value) => {
+        const email = (value || '');
+
+        if (email.split('@').length !== 2) {
+            return false;
+        } else if (email.includes(' ')) {
+            return false;
+        } else if (email[0] === '-') {
+            return false;
+        }
+
+        return true;
+    },
+};
